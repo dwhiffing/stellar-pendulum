728e58ee3563bf065fb3fe7ff21e75265096169b
Daniel Whiffing <danielwhiffing@gmail.com>
2018-03-14 12:39:10 -0400

    Adds package-lock

--- src/entities/ui.js
+++ src/entities/ui.js
@@ -13,15 +13,38 @@ export default class UserInterface {
     this.score = this.timer = this.combo = 0
 
     this.sndComboEnd = game.add.audio('comboend')
-    this.scoreText = game.add.text(10, 10, "0", { font: "bold 32px Arial", fill: "#fff" })
-    this.timeText = game.add.text(game.world.width-120, 10, "T: 2000", { font: "bold 32px Arial", fill: "#fff" })
-    this.font = game.add.retroFont('0', 62, 50, Phaser.RetroFont.TEXT_SET6, 10, 2, 2)
+    this.scoreText = game.add.text(10, 10, '0', {
+      font: 'bold 32px Arial',
+      fill: '#fff',
+    })
+    this.timeText = game.add.text(game.world.width - 120, 10, 'T: 2000', {
+      font: 'bold 32px Arial',
+      fill: '#fff',
+    })
+    this.font = game.add.retroFont(
+      '0',
+      62,
+      50,
+      Phaser.RetroFont.TEXT_SET6,
+      10,
+      2,
+      2
+    )
 
-    this.comboImage = game.add.image(game.world.centerX, game.world.centerY, this.font)
+    this.comboImage = game.add.image(
+      game.world.centerX,
+      game.world.centerY,
+      this.font
+    )
     this.comboImage.anchor.set(0.5, 1)
     this.comboImage.alpha = 0
 
-    this.comboBar = new TimerBar(game, game.world.centerX, game.world.centerY+20, 3000)
+    this.comboBar = new TimerBar(
+      game,
+      game.world.centerX,
+      game.world.centerY + 20,
+      3000
+    )
 
     this.redEmitter = this.createEmitter(100, 0xff0000)
     this.blueEmitter = this.createEmitter(100, 0x0000ff)
@@ -33,13 +56,13 @@ export default class UserInterface {
     emitter.makeParticles('bit')
     emitter.setAlpha(0.9, 0, 4000)
     emitter.gravity = 400
-    emitter.forEach(particle => particle.tint = tint)
+    emitter.forEach(particle => (particle.tint = tint))
   }
 
   emit(x, y, tint, speed) {
     this.emitter.x = x
     this.emitter.y = y
-    this.emitter.forEach(particle => particle.tint = tint)
+    this.emitter.forEach(particle => (particle.tint = tint))
     this.emitter.start(true, PARTICLE_LIFETIME, null, speed)
   }
 
@@ -59,9 +82,16 @@ export default class UserInterface {
     } else {
       this.emitter = this.blueEmitter
     }
-    this.emitter.setScale(sprite.scale.x * 0.8, sprite.scale.x * 0.5, sprite.scale.x * 0.8, sprite.scale.x * 0.5, 3000, Phaser.Easing.Quintic.Out)
-    this.emitter.setXSpeed(-200 + velocity.x*0.2, 200+velocity.x*0.3)
-    this.emitter.setYSpeed(-200 + velocity.y*0.2, 200+ velocity.y*0.3)
+    this.emitter.setScale(
+      sprite.scale.x * 0.8,
+      sprite.scale.x * 0.5,
+      sprite.scale.x * 0.8,
+      sprite.scale.x * 0.5,
+      3000,
+      Phaser.Easing.Quintic.Out
+    )
+    this.emitter.setXSpeed(-200 + velocity.x * 0.2, 200 + velocity.x * 0.3)
+    this.emitter.setYSpeed(-200 + velocity.y * 0.2, 200 + velocity.y * 0.3)
     this.emit(sprite.x, sprite.y, sprite.tint, speed)
     this.reseting = false
 
@@ -78,7 +108,7 @@ export default class UserInterface {
     }
   }
 
-  resetCombo(hard=false) {
+  resetCombo(hard = false) {
     this.reseting = true
     if (hard) {
       this.combo = 1
@@ -87,7 +117,10 @@ export default class UserInterface {
   }
 
   createScoreText(x, y, score) {
-    let text = this.game.add.text(x, y, score, { font: `bold ${Math.min(Math.ceil(14 + score*0.02), 20)}px Arial`, fill: "#fff" })
+    let text = this.game.add.text(x, y, score, {
+      font: `bold ${Math.min(Math.ceil(14 + score * 0.02), 20)}px Arial`,
+      fill: '#fff',
+    })
     setTimeout(() => {
       this.game.world.remove(text)
     }, 1500)
@@ -96,7 +129,7 @@ export default class UserInterface {
   update() {
     if (this.reseting) {
       this.timer--
-      if (this.timer <= 0 ) {
+      if (this.timer <= 0) {
         this.timer = 5
         this.combo--
         this.sndComboEnd.play()
